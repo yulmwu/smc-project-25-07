3bbd4a7257760370b18d8a3b8271fac07fc076a3
Kim Jun Young <rlawnsdud0422@gmail.com>
2025-07-07 22:31:28 +0900

    updated Backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
-async function bootstrap() {
+const bootstrap = async () => {
     const app = await NestFactory.create(AppModule)
-    await app.listen(process.env.PORT ?? 3000)
+    await app.listen(process.env.PORT ?? 3000, '0.0.0.0')
 }
 
 bootstrap()
